21d189294b0386fdd8be61dd7692da1384b8fb73
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-08-06 12:43:39 +0700

    fix: Reference Error 2

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/common/main-footer";
 import AboutUs from "@/components/home/about-us";
 import Explore from "@/components/home/home-explore";
-import Hero from "@/components/home/hero";
+import Hero from "@/components/home/home-hero";
 
 export default function Home() {
   const data = "";
