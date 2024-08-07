cb8aeea72204257f2ebd0193bcb592b6dd02f183
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-08-07 10:31:23 +0700

    fix: Profile Page Export

--- src/app/dashboard/user/profile/page.tsx
+++ src/app/dashboard/user/profile/page.tsx
@@ -1,7 +1,9 @@
 const Profile = () => {
-    return (
-        <section className="pt-[7rem] md:pl-[17rem]">
-            <h1 className="text-2xl font-bold ml-4">Histories</h1>
-        </section>
-    )
-}
+  return (
+    <section className="pt-[7rem] md:pl-[17rem]">
+      <h1 className="text-2xl font-bold ml-4">Profile</h1>
+    </section>
+  );
+};
+
+export default Profile;
