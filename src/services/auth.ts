47d2f998fa5021d5d249ead20922d8c5d1f5033a
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-08-07 10:40:50 +0700

    fix: auth service reference error 2

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,7 +2,7 @@ import { PasswordResetRequest, PasswordResetResponse } from "../types/sign/reset
 import { axiosInstance } from "../services/api-config";
 import { VerifyPasswordResetRequest, VerifyPasswordResetResponse } from "../types/sign/verify-password";
 import { ChangePasswordRequest, ChangePasswordResponse } from "../types/sign/change-password";
-import { RegisterUserRequest, RegisterUserResponse } from "../types/sign/register";
+import { RegisterUserRequest, RegisterUserResponse } from "../types/sign/register-user";
 import { LoginRequest, LoginResponse } from "../types/sign/login";
 
 export const registerUser = async (data: RegisterUserRequest): Promise<RegisterUserResponse> => {
