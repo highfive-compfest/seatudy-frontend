9480292f69a108828e0ab1dffba330bf0d300de9
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-08-08 10:10:08 +0700

    fix: commenting course map in dashboard for now

--- src/app/dashboard/user/courses/page.tsx
+++ src/app/dashboard/user/courses/page.tsx
@@ -6,9 +6,9 @@ const Courses = () => {
     <section className="pt-[7rem] md:pl-[17rem]">
       <h1 className="text-2xl font-bold ml-4">Courses</h1>
       <div className="mt-4 flex gap-4 flex-wrap justify-center md:justify-start">
-        {courses.map((course, idx) => (
+        {/* {courses.map((course, idx) => (
           <CourseCard key={idx} course={course} />
-        ))}
+        ))} */}
       </div>
     </section>
   );
