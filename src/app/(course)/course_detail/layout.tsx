6bfb13f696583c32347156d079a87b747c2369ac
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-08-08 10:24:18 +0700

    fix: adding suspense

---
+++ src/app/(course)/course_detail/layout.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode, Suspense } from "react";
+import Navbar from "@/components/common/main-navbar";
+import Footer from "@/components/common/main-footer";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <div className="container mx-auto pt-24 px-8 pb-32">
+        <Suspense fallback={<></>}>{children}</Suspense>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
