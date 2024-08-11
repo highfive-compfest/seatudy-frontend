c64a1eee42278cea2b9cb0c1ad99053c86d514d0
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-08-11 20:46:44 +0700

    fix: apos problem

--- src/app/dashboard/instructor/manage/[idCourse]/page.tsx
+++ src/app/dashboard/instructor/manage/[idCourse]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { EditCourse } from "@/components/dashboard/instructor/edit-form";
 import { deleteCourseById, getCourseById } from "@/services/course";
 import { getSegment } from "@/utils/utils";
@@ -11,67 +11,68 @@ import { HeaderCourse } from "@/components/dashboard/instructor/header";
 import { Material } from "@/components/dashboard/instructor/material/material";
 
 const Manage = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const navs = ["Material", "Forum", "Edit"]
+  const navs = ["Material", "Forum", "Edit"];
 
-    const [course, setCourse] = useState<Course>()
-    const [isPending, setPending] = useState(false)
-    const [activeSection, setActiveSection] = useState("Material");
+  const [course, setCourse] = useState<Course | undefined>(undefined);
+  const [isPending, setPending] = useState(false);
+  const [activeSection, setActiveSection] = useState("Material");
 
-    const accToken = getCookie("authToken") as string;
+  const accToken = getCookie("authToken") as string;
 
-    const pathname = usePathname();
-    const id = getSegment(pathname, 4)
-    
-    useEffect(()=>{
-        const fetchCourseId = async () => {
-            try {
-                const data = await getCourseById(id)
-                setCourse(data.payload)
-            } catch (error:any) {
-                console.log(error.response)
-            }
-        }
-        fetchCourseId()
-    },[])
+  const pathname = usePathname();
+  const id = getSegment(pathname, 4);
 
-    const handleDelete = async () => {
-        setPending(true)
-        try {
-            await deleteCourseById(accToken,id)
-            setPending(false)
-            router.replace("/dashboard/instructor/manage")
-        } catch (error:any) {
-            console.log(error.response)
-        } finally {
-            setPending(false)
-        }
+  useEffect(() => {
+    if (!id) return;
+
+    const fetchCourseId = async () => {
+      try {
+        const data = await getCourseById(id);
+        setCourse(data.payload);
+      } catch (error: any) {
+        console.log(error.response);
+      }
+    };
+
+    fetchCourseId();
+  }, [id]);
+
+  const handleDelete = async () => {
+    if (!id || !accToken) return;
+
+    setPending(true);
+    try {
+      await deleteCourseById(accToken, id);
+      router.replace("/dashboard/instructor/manage");
+    } catch (error: any) {
+      console.log(error.response);
+    } finally {
+      setPending(false);
     }
+  };
 
-    return (
-        <section className="p-4 pt-28 w-full">
-            <HeaderCourse course={course}/>
-            <nav className="py-6 px-4 bg-white rounded-lg shadow-md flex gap-2 overflow-auto">
-            {navs.map((content, idx)=>(
-                <button
-                    key={idx} 
-                    onClick={() => setActiveSection(content)}
-                    className={`block w-fit text-left py-2 px-4 rounded-md ${activeSection === content? "bg-gray-300" : "bg-gray-100"}`}
-                    >
-                        {content}
-                </button>
-            ))}
-           {isPending?<Spinner/>:
-           (<button onClick={handleDelete} className="block w-fit text-left py-2 px-4 rounded-md bg-red-500 text-white">
-                Delete
-           </button>)}
-            </nav>
-            {activeSection === "Edit"?<EditCourse course={course}/>:
-            activeSection === "Material" && course?<Material courseId={course.id}/>:null
-            }
-        </section>
-    )
-}
+  return (
+    <section className="p-4 pt-28 w-full">
+      <HeaderCourse course={course} />
+      <nav className="py-6 px-4 bg-white rounded-lg shadow-md flex gap-2 overflow-auto">
+        {navs.map((content, idx) => (
+          <button key={idx} onClick={() => setActiveSection(content)} className={`block w-fit text-left py-2 px-4 rounded-md ${activeSection === content ? "bg-gray-300" : "bg-gray-100"}`}>
+            {content}
+          </button>
+        ))}
+        {isPending ? (
+          <Spinner />
+        ) : (
+          <button onClick={handleDelete} className="block w-fit text-left py-2 px-4 rounded-md bg-red-500 text-white">
+            Delete
+          </button>
+        )}
+      </nav>
+      {activeSection === "Edit" ? <EditCourse course={course} /> : activeSection === "Material" && course ? <Material courseId={course.id} /> : null}
+    </section>
+  );
+};
 
-export default Manage
+export default Manage;
