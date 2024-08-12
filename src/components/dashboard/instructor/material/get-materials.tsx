d426db0f82ac9f24891166ca9efe1a804ad40a65
Dindin Imanudin <dindin.imanudin@mhs.itenas.ac.id>
2024-08-12 09:50:11 +0700

    delete material

--- src/components/dashboard/instructor/material/get-materials.tsx
+++ src/components/dashboard/instructor/material/get-materials.tsx
@@ -6,18 +6,18 @@ import { FaBook } from "react-icons/fa6";
 import { MdBook } from "react-icons/md";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { ActionButton } from "./action";
+
 
 export const GetMaterials = ({courseId}:{courseId:string}) => {
     const [materials, setMaterials] = useState<Material[]>()
-
+    
     const pathname = usePathname()
-    console.log(pathname)
-
+    
     useEffect(()=>{
         const getMaterials = async () => {
             try {
                 const res = await getMaterialByCourse(courseId)
-                console.log(res.payload)
                 setMaterials(res.payload)
             } catch (error:any) {
                 console.error(error.response)
@@ -35,17 +35,20 @@ export const GetMaterials = ({courseId}:{courseId:string}) => {
                     const updatedDate = new Date(materi.updated_at).toLocaleDateString();
 
                     return(
-                        <Link href={`${pathname}/${materi.id}`} className="border-2 border-gray-200 px-4 py-3 rounded-lg flex gap-2 items-center" key={idx}>
-                            <div className="p-2 bg-blue-500 rounded-full w-fit"><MdBook size={23} color="white"/></div>
-                            <div>
-                                <h3 className="font-bold">{materi.title}</h3>
-                                <small className="block">{
-                                    createdDate !== updatedDate?
-                                    `${updatedDate} (updated)`:
-                                    createdDate
-                                }</small>
-                            </div>
-                        </Link>
+                        <div className="relative">
+                            <Link href={`${pathname}/${materi.id}`} className="hover:bg-gray-200 border-2 border-gray-200 px-4 py-3 rounded-lg flex gap-2 items-center" key={idx}>
+                                <div className="p-2 bg-blue-500 rounded-full w-fit"><MdBook size={23} color="white"/></div>
+                                <div>
+                                    <h3 className="font-bold">{materi.title}</h3>
+                                    <small className="block">{
+                                        createdDate !== updatedDate?
+                                        `${updatedDate} (updated)`:
+                                        createdDate
+                                    }</small>
+                                </div>
+                            </Link>
+                            <ActionButton materiId={materi.id}/>
+                        </div>
                     )
                 })
             }</div>
