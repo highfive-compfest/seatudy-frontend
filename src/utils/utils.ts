63688b95f6cd95e0e038366ed7f5a6ae8ddcc720
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-08-14 18:37:17 +0700

    fix: utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -19,19 +19,20 @@ export const dummyCourse: Course = {
   rating: 0,
   review_count: 0,
   materials: [],
+  assignments: [],
 };
 
 export const getExtFile = (url: string) => {
-  return url.split('.').pop()?.toLowerCase()
+  return url.split(".").pop()?.toLowerCase();
 };
 
 export const getTimeNow = () => {
   const now = new Date();
   const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, '0');
-  const day = String(now.getDate()).padStart(2, '0');
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
   const formattedDate = `${year}-${month}-${day}T${hours}:${minutes}`;
-  return formattedDate
+  return formattedDate;
 };
