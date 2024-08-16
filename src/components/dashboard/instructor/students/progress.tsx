183ef05c18a5ef0d05287c6c33f9792c942303bf
Dindin Imanudin <dindin.imanudin@mhs.itenas.ac.id>
2024-08-16 09:50:10 +0700

    progress bar

--- src/components/dashboard/instructor/students/progress.tsx
+++ src/components/dashboard/instructor/students/progress.tsx
@@ -1,31 +1,28 @@
-// import { getStudentProgres } from "@/services/course";
+import { getStudentProgres } from "@/services/course";
 import { getCookie } from "cookies-next";
 import { useEffect, useState } from "react";
 
 export const ProgresBar = ({studentId, courseId}:{studentId:string, courseId:string}) => {
     const [value, setValue] = useState(0)
-    const [id, setId] = useState("")
     const accToken = getCookie("authToken") as string
 
-    // useEffect(()=>{
-    //     const getProgress = async () => {
-    //         try {
-    //             const res = await getStudentProgres(courseId, studentId, accToken)
-    //             setValue(res.payload)
-    //             console.log(res.payload)
-    //         } catch (error:any) {
-    //             console.error(error.response)
-    //         }
-    //     }
-    //     getProgress()
-    // },[studentId])
-
-    console.log(value)
+    useEffect(()=>{
+        const getProgress = async () => {
+            try {
+                const res = await getStudentProgres(courseId, studentId, accToken)
+                setValue(res.payload)
+                console.log(res.payload)
+            } catch (error:any) {
+                console.error(error.response)
+            }
+        }
+        getProgress()
+    },[studentId])
 
     return (
-        <div className="flex gap-1">
-            <div className="bg-gray-300 h-4 rounded-full overflow-hidden">
-                <div className="bg-blue-600 h-full w-[20rem]"></div>
+        <div className="flex gap-1 mt-1">
+            <div className="bg-gray-300 h-4 rounded-full overflow-hidden w-full">
+                <div className={`bg-blue-500 h-full w-[${value.toString()}%]`}></div>
             </div>
             <small>{value}%</small>
         </div>
